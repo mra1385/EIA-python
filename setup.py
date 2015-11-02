dae1b751affaa5370a93f122ebb13e9ebbee0d98
mra1385 <mikeazar@gmail.com>
2015-11-02 16:17:17 -0500

    "added setup.py"

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='EIA_python',
+    version='1.0',
+    packages=['EIA', 'build.lib.EIA'],
+    url='https://github.com/mra1385/EIA-python',
+    license='MIT',
+    author='Mike Azar',
+    author_email='mikeazar@gmail.com',
+    description='Python Wrapper for EIA API'
+)
