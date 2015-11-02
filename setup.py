899ac8538e89bc1d4ea42b29f3b545bca3eedf77
mra1385 <mikeazar@gmail.com>
2015-11-02 16:58:01 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     packages=['EIA', 'build.lib.EIA'],
     url='https://github.com/mra1385/EIA-python',
-    license='MIT',
+    license='BSD',
     author='Mike Azar',
     author_email='mikeazar@gmail.com',
     description='Python Wrapper for EIA API'
