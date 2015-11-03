f58f10230dae772797e398e077b050716854e375
mra1385 <mikeazar@gmail.com>
2015-11-02 20:47:43 -0500

    "updated setup file"

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     name='EIA-python',
     version='1.1',
-    packages=['EIA', 'build.lib.EIA'],
+    packages=['EIA', 'build.lib.EIA', 'build.lib.build.lib.EIA'],
     url='https://github.com/mra1385/EIA-python',
-    download_url = 'https://github.com/mra1385/EIA-python',
+    download_url='https://github.com/mra1385/EIA-python',
     license='BSD',
     author='Mike Azar',
     author_email='mikeazar@gmail.com',
