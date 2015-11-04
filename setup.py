b6000f64dc998375a9648b5a4dacc928ab8bda02
mra1385 <mikeazar@gmail.com>
2015-11-03 23:03:59 -0500

    "updated README"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='EIA-python',
-    version='1.5',
+    version='1.6',
     packages=['EIA'],
     url='https://github.com/mra1385/EIA-python',
     license='BSD',
