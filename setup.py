25da3cd66dd1a81dc0198a35ee391426147b9fc3
mra1385 <mikeazar@gmail.com>
2015-11-04 13:10:52 -0500

    "refactored code"

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='EIA-python',
-    version='1.8',
-    packages=['eia'],
-    url='https://github.com/mra1385/eia-python',
+    version='1.9',
+    packages=['eia', 'build.lib.eia'],
+    url='https://github.com/mra1385/EIA-python',
     license='BSD',
     author='Mike Azar',
     author_email='mikeazar@gmail.com',
-    description='Python Wrapper for Energy Information Administration API'
+    description='Python Wrapper for the Energy Information Administration (EIA) API'
 )
