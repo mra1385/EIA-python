b9f3bf782e1acc0f85dd8ce41dae6b546d65a89c
mra1385 <mikeazar@gmail.com>
2015-11-04 13:27:05 -0500

    "added setup.py"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='EIA-python',
-    version='1.9',
+    version='1.10',
     packages=['eia'],
     url='https://github.com/mra1385/EIA-python',
     license='BSD',
