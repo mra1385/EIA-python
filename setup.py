80e76da45ff3be1531b005f6f45ab6b89ecf8684
mra1385 <mikeazar@gmail.com>
2015-11-04 13:26:23 -0500

    "added setup.py"

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='EIA-python',
+    version='1.9',
+    packages=['eia'],
+    url='https://github.com/mra1385/EIA-python',
+    license='BSD',
+    author='Mike Azar',
+    author_email='mikeazar@gmail.com',
+    description='Python Wrapper for the Energy Information Administration (EIA) API'
+)
