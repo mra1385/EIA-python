49a52355eb729690d86a3390c183a3813cd1e314
mra1385 <mikeazar@gmail.com>
2015-11-04 22:47:22 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='EIA-python',
-    version='1.12',
+    version='1.13',
     packages=['eia'],
     url='https://github.com/mra1385/EIA-python',
     license='BSD',
